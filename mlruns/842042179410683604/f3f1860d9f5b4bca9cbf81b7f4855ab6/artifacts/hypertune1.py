08971591ee845e7b2bf7d4bc509a6ae127d56f8f
sivanagasingu <sivanag123@gmail.com>
2025-05-05 20:12:52 -0400

    Final code commited

--- mlruns/842042179410683604/f3f1860d9f5b4bca9cbf81b7f4855ab6/artifacts/hypertune1.py
+++ mlruns/842042179410683604/f3f1860d9f5b4bca9cbf81b7f4855ab6/artifacts/hypertune1.py
@@ -25,6 +25,18 @@ param_grid = {
 # Applying GridSearchCV
 grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=5, n_jobs=-1, verbose=2)
 
+# # Run without MLflow from here
+# grid_search.fit(X_train, y_train)
+
+# # Displaying the best params and best score
+# best_params = grid_search.best_params_
+# best_score = grid_search.best_score_
+
+# print(best_params)
+# print(best_score)
+# # Till here
+
+
 mlflow.set_experiment('breast-cancer-rf-hp')
 
 with mlflow.start_run() as parent:
